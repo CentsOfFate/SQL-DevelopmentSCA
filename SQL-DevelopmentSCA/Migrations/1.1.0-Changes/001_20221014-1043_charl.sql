43618f18c8cf67827f799ede553bdfa167d12fc7
CentsOfFate <charlie.dalldorf@gmail.com>
2022-10-14 10:48:51 -0500

    Changes

---
+++ SQL-DevelopmentSCA/Migrations/1.1.0-Changes/001_20221014-1043_charl.sql
@@ -0,0 +1,8 @@
+﻿-- <Migration ID="57ec5fcc-8e9e-4923-9be9-a485acdce04a" />
+GO
+
+PRINT N'Altering [dbo].[Customer]'
+GO
+ALTER TABLE [dbo].[Customer] ADD
+[AccountType] [varchar] (2) NULL
+GO
---
+++ SQL-DevelopmentSCA/SQLChangeAutomationArtifacts/Project/Migrations/1.0.0-Baseline/001_20221014-1015_charl.sql
@@ -0,0 +1,12 @@
+﻿-- <Migration ID="89689a13-afcc-4cc0-a7ba-f1c9abe827a8" />
+GO
+
+PRINT N'Creating [dbo].[Customer]'
+GO
+CREATE TABLE [dbo].[Customer]
+(
+[Id] [int] NULL,
+[Name] [varchar] (50) NULL,
+[AccountNumber] [varchar] (50) NULL
+)
+GO
